327fde0411947aec9869a5cf98cdb67249ce1604
Merna-M-Hassan <merna.abdelfatah.hassan@gmail.com>
2026-08-17 13:04:39 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_8_C__OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
