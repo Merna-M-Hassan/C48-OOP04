a2af70d18ba5176e0c28e9f30c3a330567002557
Merna-M-Hassan <merna.abdelfatah.hassan@gmail.com>
2026-08-18 00:36:45 +0300

    Question 2)

--- Program.cs
+++ Program.cs
@@ -23,6 +23,35 @@
              * It is the starting point for all OOP design.
              */
             #endregion
+
+            #region Q2) a)
+            // What is the difference between an Abstract Class and an Interface?
+
+            /*
+             * Abstract class: can have fields, can have constructors, can use public, private, and protected access modifiers, class can inherite one class only.
+             * Serves as a base for other classes and can not be instantiated.
+             * Use abstract class when classes are related and share code.
+             * 
+             * Inferface: can not have fields, can not have constructors, can only use public access modifier, class can implement mutiple interfaces.
+             * A contract that classes must follow. It contains only method signatures (without implementation).
+             */
+            #endregion
+
+            #region Q2) b)
+            //When would you choose an Interface instead of an Abstract Class?
+
+            /*
+             *Use interface when unrelated classses need to implement the same behavior. Also, if a multiple behaviors is need to be implemented.
+             */
+            #endregion
+
+            #region Q2) c)
+            //Can a class inherit from multiple abstract classes? Can it implement multiple interfaces?
+
+            /*
+             * No, a class can not inherit from multiple abstract classes. Yes, can implement multiple interface, because interface supports multiple implementation.
+             */
+            #endregion
         }
     }
 }
