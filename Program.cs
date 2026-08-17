9cf874c0a84293478affd802fed49eafb13feeb8
Merna-M-Hassan <merna.abdelfatah.hassan@gmail.com>
2026-08-17 20:10:17 +0300

    Question 1)

--- Program.cs
+++ Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1) a)
+            //What is Abstraction in Object-Oriented Programming?
+
+            /*
+             * Abstraction: hiding complex implementation details and showing only the essential features of an object.
+             * Showing only what's necessary and hiding how it's done.
+             * Focusing on what an object does, not how it does it
+             */
+            #endregion
+
+            #region Q1) b)
+            //Why is abstraction considered one of the four pillars of OOP?
+
+            /*
+             * Because it simplifies complexity by focusing on what the object does (what the object does), not the complex how it does.
+             * Also provides a blueprint for other pillars. Enables encapsulation (defines "what" and encapsulation defines "how"), polymorphism (provides the common interface for different implementations), and inheritance (provides the blueprint for class hierarchies).
+             * It is the starting point for all OOP design.
+             */
+            #endregion
         }
     }
 }
